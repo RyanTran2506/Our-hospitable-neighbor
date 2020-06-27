866b53088594cf780dd0a4e282467b5caaefa0de
Andy Nguyen <andynguyen.swdev@gmail.com>
2020-06-26 22:43:16 -0700

    Add Google Maps SDK

--- app/src/main/java/com/example/ourhospitableneighbor/MainActivity.java
+++ app/src/main/java/com/example/ourhospitableneighbor/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.ourhospitableneighbor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(map -> this.map = map);
     }
 }
