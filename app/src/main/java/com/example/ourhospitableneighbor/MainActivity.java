d164f9d9094de6158871639c5794e663b4cc3cee
Andy Nguyen <andynguyen.swdev@gmail.com>
2020-06-27 17:08:42 -0700

    Implement navigation drawer

--- app/src/main/java/com/example/ourhospitableneighbor/MainActivity.java
+++ app/src/main/java/com/example/ourhospitableneighbor/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.ourhospitableneighbor;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
     private GoogleMap map;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,37 @@ public class MainActivity extends AppCompatActivity {
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(map -> this.map = map);
+
+        setUpNavigationDrawer();
+        setUpLogOutButton();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Close the drawer if it's open, otherwise close the app
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void setUpNavigationDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+
+        // Need to call this function to change the back icon into the hamburger icon
+        toggle.syncState();
+    }
+
+    private void setUpLogOutButton() {
+        Button logOut = findViewById(R.id.logout);
+        logOut.setOnClickListener(v -> {
+            Toast.makeText(this, "Log out", Toast.LENGTH_SHORT).show();
+        });
     }
 }
